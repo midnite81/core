a35036dbf3eaf5f9c958dc937ddc4487794cb5de
Simon Rogers <webdev@midnite.uk>
2022-08-22 23:22:38 +0100

    [Main] Add QueryBuilder to Builder::getQueries signature

--- src/Eloquent/Builder.php
+++ src/Eloquent/Builder.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Midnite81\Core\Eloquent;
 
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
+use Illuminate\Database\Query\Builder as BuilderAlias;
 
 class Builder
 {
-    public static function getQueries(EloquentBuilder $builder): string
+    public static function getQueries(EloquentBuilder|BuilderAlias $builder): string
     {
         $addSlashes = str_replace(['?', '%'], ["'?'", '%%'], $builder->toSql());
 
