7fa6ebe776c001da6d65f50ffebfdaf9ce0343b2
Simon Rogers <webdev@midnite.uk>
2023-02-16 13:32:34 +0000

    [feature/count] Adds count class in

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Midnite81\Core;
 
 use Illuminate\Support\ServiceProvider;
+use Midnite81\Core\Contracts\Services\CounterInterface;
 use Midnite81\Core\Contracts\Services\ExecuteInterface;
 use Midnite81\Core\Contracts\Services\UuidGeneratorInterface;
+use Midnite81\Core\Services\Counter;
 use Midnite81\Core\Services\Execute;
 use Midnite81\Core\Services\UuidGenerator;
 
@@ -34,6 +36,7 @@ class CoreServiceProvider extends ServiceProvider
         $this->app->bind(UuidGeneratorInterface::class, UuidGenerator::class);
         $this->app->alias(UuidGeneratorInterface::class, 'm81-uuid');
         $this->app->bind(ExecuteInterface::class, Execute::class);
+        $this->app->bind(CounterInterface::class, Counter::class);
 
         $this->mergeConfigFrom(__DIR__ . '/../config/core-ignition.php', 'core-ignition');
     }
@@ -47,6 +50,7 @@ class CoreServiceProvider extends ServiceProvider
             UuidGeneratorInterface::class,
             ExecuteInterface::class,
             'm81-uuid',
+            CounterInterface::class
         ];
     }
 }
---
+++ src/Services/Counter.php
@@ -0,0 +1,124 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Midnite81\Core\Services;
+
+use Midnite81\Core\Contracts\Services\CounterInterface;
+
+/**
+ * A simple class that keeps track of a count
+ */
+class Counter implements CounterInterface
+{
+    protected int $count = 0;
+
+    /**
+     * Sets the starting point of the counter
+     *
+     * @param int $value
+     * @return $this
+     */
+    public function startingPoint(int $value): static
+    {
+        $this->count = $value;
+        return $this;
+    }
+
+    /**
+     * Gets the current count
+     *
+     * @return int
+     */
+    public function get(): int
+    {
+        return $this->count;
+    }
+
+    /**
+     * Alias of get
+     *
+     * @return int
+     */
+    public function getCurrent(): int
+    {
+        return $this->get();
+    }
+
+    /**
+     * Returns if the current count equals the passed value
+     *
+     * @param int $value
+     * @return bool
+     */
+    public function equals(int $value): bool
+    {
+        return $this->count === $value;
+    }
+
+    /**
+     * Increments count by 1
+     *
+     * @return $this
+     */
+    public function next(): static
+    {
+        return $this->increase(1);
+    }
+
+    /**
+     * Increments the count by what ever value is specified
+     *
+     * @param int $value
+     * @return $this
+     */
+    public function incrementBy(int $value): static
+    {
+        return $this->increase($value);
+    }
+
+    /**
+     * Decreases the count by what ever value is specified
+     *
+     * @param int $value
+     * @return $this
+     */
+    public function decrementBy(int $value): static
+    {
+        return $this->decrease($value);
+    }
+
+    /**
+     * Decreases the count by 1
+     *
+     * @return $this
+     */
+    public function previous(): static
+    {
+        return $this->decrease(1);
+    }
+
+    /**
+     * Increases the value by the specified value
+     *
+     * @param int $value
+     * @return $this
+     */
+    protected function increase(int $value): static
+    {
+        $this->count = $this->count + $value;
+        return $this;
+    }
+
+    /**
+     * Decreases the value by the specified value
+     *
+     * @param int $value
+     * @return $this
+     */
+    protected function decrease(int $value): static
+    {
+        $this->count = $this->count - $value;
+        return $this;
+    }
+}
