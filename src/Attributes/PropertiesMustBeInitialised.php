48370ccdc4a0d5915e44feae754d025418d85bb7
Simon Rogers <webdev@midnite.uk>
2023-03-29 17:56:51 +0100

    [feature/carbon-formatting-on-entities] Adds an attribute to format Carbon properties. Annotates all attributes.

--- src/Attributes/PropertiesMustBeInitialised.php
+++ src/Attributes/PropertiesMustBeInitialised.php
@@ -6,6 +6,13 @@ namespace Midnite81\Core\Attributes;
 
 use Attribute;
 
+/**
+ * This attribute is used to define a class as requiring all properties to be initialised
+ *
+ * @Annotation
+ *
+ * @Target({"CLASS"})
+ */
 #[Attribute(Attribute::TARGET_CLASS)]
 class PropertiesMustBeInitialised
 {
--- src/Attributes/PropertyName.php
+++ src/Attributes/PropertyName.php
@@ -6,6 +6,13 @@ namespace Midnite81\Core\Attributes;
 
 use Attribute;
 
+/**
+ * This attribute is used to define the name of a property (as opposed to the name of the property in the class)
+ *
+ * @Annotation
+ *
+ * @Target({"PROPERTY"})
+ */
 #[Attribute(Attribute::TARGET_PROPERTY)]
 class PropertyName
 {
